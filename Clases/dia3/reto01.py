86d26e479d0a7fff29120762d4420cd7c7538862
robertonavarroc <elsacarrizalesguzman@gmail.com>
2025-12-06 13:48:25 -0500

    python 06-12-2025

---
+++ Clases/dia3/reto01.py
@@ -0,0 +1,36 @@
+# RETO 1 : CREAR UN PROGRAMA USANDO COMO EJEMPLO EL CODIGO DE LA CALCULADORA 
+# QUE PERMITA CONVERTIR EL VALOR DE UNA MONEDA DE SOLES A DOLARES Y VICEVERSA,
+# POR EJEMPLO SI INGRESO CONVERTIR SOLES A DOLARES E INGRESO 3000 SOLES 
+# DEBERIA MOSTRARME SU VALOR EN DOLARES QUE SERIA 1000 DOLARES CONSIDERANDO 
+# QUE EL TIPO DE CAMBIO ES 3
+
+import os
+
+salir = "no"
+
+while salir == "no":
+    os.system("cls")
+    
+    print("======== [TIPO DE CAMBIO] ===========")
+    print("======== Tipo de cambio 3.0 =========")
+    print("=====================================")
+    moneda = round(float(input("Ingrese el monto a cambiar: ")),2)
+    
+    print("========== [OPCIONES] =============")
+    print("1.- SOLES A DOLARES")
+    print("2.- DOLARES A SOLES")
+    opcion = input("Ingresar opcion 1 o 2: ")
+    
+    if opcion == "1":
+        cambio = float(moneda / 3)
+        print(f"{moneda} de soles a dolares: {round(cambio,2)}")
+    elif opcion == "2":
+        cambio = float(moneda * 3)
+        print(f"{moneda} de solares a soles: {round(cambio,2)}")
+    else:
+        print("Opcion Invalida")
+    
+    salir = input("Desea salir si/no: ")
+    if salir == "si":
+        break
+        
